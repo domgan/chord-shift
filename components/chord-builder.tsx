4b9173c6484e0f9accf767e0c453f94131496bcd
Dominik G <domiran35@gmail.com>
2023-01-14 19:38:09 +0100

    feat: generateNotes v1

--- components/chord-builder.tsx
+++ components/chord-builder.tsx
@@ -21,7 +21,7 @@ export default function ChordBuilder(props: ChooseChordProps) {
 
   const CharSelector = (props: CharSelectorProps) => {
     return (
-      <select title={props.title} onChange={e => props.setChar(e.target.value)} value={props.char} className='text-lg font-medium py-2 px-3 rounded-lg bg-gray-200 focus:outline-none'>
+      <select title={props.title} onChange={e => props.setChar(e.target.value)} value={props.char} className='hover:shadow-lg text-lg font-medium mx-0.5 py-2 px-3 rounded-lg bg-gray-200 focus:outline-none'>
         {props.blocks.map(block => <option className='py-1 px-2 hover:bg-gray-200'>{block}</option>)}
       </select>
     )
@@ -29,11 +29,11 @@ export default function ChordBuilder(props: ChooseChordProps) {
 
   const ChooseChord = () => {
     const notes = 'CDEFGAB'.split('')
-    const chromatics = ' ♯♭'.split('')
-    const tonalities = ' m'.split('')  // todo objects {name: string, label: string}
+    const chromatics = ' #b'.split('')
+    const tonalities = ['', 'm', 'aug', 'dim']  // todo objects {name: string, label: string}
 
     return (
-      <div className='flex'>
+      <div className='flex items-center justify-between'>
         <CharSelector title='Notes' blocks={notes} char={note} setChar={setNote} />
         <CharSelector title='Chromatics' blocks={chromatics} char={chromatic} setChar={setChromatic} />
         <CharSelector title='Tonality' blocks={tonalities} char={tonality} setChar={setTonality} />
@@ -41,20 +41,53 @@ export default function ChordBuilder(props: ChooseChordProps) {
     )
   }
 
-  const generateNotes = (): string[] => {
-    return ['N', 'O', 'T', 'E', 'S']
+  const generateNotes = (): string[] => {  // (rootNote: string, tonality: string, chromatics: string)
+    const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+    // normalize
+    let rootNote;
+    if (chromatic === 'b') {
+      rootNote = notes[notes.indexOf(note) - 1]
+    } else {
+      rootNote = note + chromatic
+    }
+    let chord = [rootNote];
+    let rootIndex = notes.indexOf(rootNote);
+
+    switch (tonality) {
+      case "":
+        chord.push(notes[(rootIndex + 4) % 12]);
+        chord.push(notes[(rootIndex + 7) % 12]);
+        break;
+      case "m":
+        chord.push(notes[(rootIndex + 3) % 12]);
+        chord.push(notes[(rootIndex + 7) % 12]);
+        break;
+      case "dim":
+        chord.push(notes[(rootIndex + 3) % 12]);
+        chord.push(notes[(rootIndex + 6) % 12]);
+        break;
+      case "aug":
+        chord.push(notes[(rootIndex + 4) % 12]);
+        chord.push(notes[(rootIndex + 8) % 12]);
+        break;
+      default:
+        throw new Error("Invalid tonality");
+    }
+
+    return chord;
   }
 
   const handleSave = () => {
     const label = note + chromatic + tonality
-    label && props.chords.push({ id: props.chords.length, label: note + chromatic + tonality, notes: generateNotes() })
+    label && props.chords.push({ id: props.chords.length, label: label, notes: generateNotes() })
     // props.setChords(props.chords)
     props.setShowChordBuilder(false)
   }
 
   return (
     <>
-      <div className="absolute rounded-lg p-4 m-auto max-w-lg bg-white z-20">
+      <div className="absolute rounded-lg p-4 m-auto max-w-lg z-20 shadow-md bg-gradient-to-r from-cyan-500 to-blue-500">
         <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
           <h3 className="text-lg font-medium">
             Chord Builder
@@ -65,8 +98,8 @@ export default function ChordBuilder(props: ChooseChordProps) {
         </div>
         {/* <p>{note}{chromatic}{tonality}</p> */}
         <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-          <button className='btn rounded-full' onClick={handleSave}>Save</button>
-          <button className='btn rounded-full' onClick={() => props.setShowChordBuilder(false)}>Cancel</button>
+          <button className='bg-green-500 hover:bg-green-600 hover:shadow-lg text-white font-medium p-2 rounded-lg w-24 h-12' onClick={handleSave}>Save</button>
+          <button className='bg-red-500 hover:bg-red-600 text-white p-2 rounded-lg hover:shadow-lg font-medium w-24 h-12' onClick={() => props.setShowChordBuilder(false)}>Cancel</button>
         </div>
       </div>
       <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-25 z-10"></div>
