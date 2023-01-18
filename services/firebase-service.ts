7ea95425972511418127f3911933c01ce286fd17
domgan <ganczarski.dominik@gmail.com>
2023-01-18 22:14:15 +0100

    next vercel try

--- services/firebase-service.ts
+++ services/firebase-service.ts
@@ -3,20 +3,19 @@ import { getFirestore, doc, getDoc, setDoc, Firestore } from 'firebase/firestore
 import { WorkspaceElement } from '../pages/chords'
 // import { getAnalytics } from "firebase/analytics"
 
-export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-}
-console.log(`firebaseConfig ${JSON.stringify(firebaseConfig)}`)
 
 export default class FirebaseService {
   db: Firestore
 
   constructor() {
+    const firebaseConfig = {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+    }
     this.db = getFirestore(initializeApp(firebaseConfig))
   }
 
